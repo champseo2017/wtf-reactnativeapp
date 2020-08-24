c7bf0f0c9cca856ec4d34add7dd50b1a904925c1
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-08-25 00:40:23 +0700

    update

---
+++ src/containers/AddTodo.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, TextInput } from 'react-native'
+
+class AddTodo extends Component {
+    render() {
+        return (
+           <View style={{
+               flexDirection:'row'
+           }}>
+                 
+          </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    }
+})
+
+export default AddTodo
