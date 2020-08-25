2934449551813746f52e8689459caf3b95c11179
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-08-26 06:59:35 +0700

    todoApp #4
    todoApp #4

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import AddTodo from './containers/AddTodo'
-
-export class TodoApp extends Component {
+import VisibleTodos from './containers/VisibleTodos'
+class TodoApp extends Component {
+   state = {
+     todos:[],
+     visibilityFilter:'SHOW_ALL_TODOS'
+   }
   render() {
     return (
       <View style={styles.container}>
         <AddTodo/>
+        <View>
+          <VisibleTodos/>
+        </View>
       </View>
     );
   }
@@ -15,8 +22,7 @@ export class TodoApp extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40
   },
 });
 
