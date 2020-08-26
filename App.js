5dbac23d7887106045c4138b71d35889203ef52e
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-08-26 21:59:11 +0700

    Practical Redux Thunk | React Native | Async Actions | WTF is Redux
    Practical Redux Thunk | React Native | Async Actions | WTF is Redux

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import TodoApp from "./src/TodoApp";
-import store from "./src/store";
-import { Provider } from "react-redux";
-export default class App extends React.Component {
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+import ReduxAsyncApp from './app/ReduxAsyncApp'
+import { Provider } from 'react-redux'
+import store from './app/store'
+
+class App extends Component {
+
   render() {
     return (
       <Provider store={store}>
-        <TodoApp />
+        <ReduxAsyncApp />
       </Provider>
     );
   }
 }
 
+export default App
+
+// export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
